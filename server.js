5f227d8b5e13af2d4e345e566d23a519bab5ad8a
Kade-95 <ikeka95@gmail.com>
2021-02-24 19:35:12 +0100

    Initial Commit

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 app.use(express.static('./dist/app'));
 
 app.get('/*', (req, res) =>
-    res.sendfile('index.html', { root: 'dist/App' })
+    res.sendFile('index.html', { root: 'dist/app' })
 );
 
 app.listen(process.env.PORT || 4200);
